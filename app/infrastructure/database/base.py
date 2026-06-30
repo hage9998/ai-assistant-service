1436638015deb3073fbdb8a8f6e9825031154b8c
hage9998 <lucashage23@gmail.com>
2026-06-30 18:54:43 -0300

    feat: add declarative base

---
+++ app/infrastructure/database/base.py
@@ -0,0 +1,7 @@
+"""Base declarativa para todos os modelos ORM da aplicação."""
+
+from sqlalchemy.orm import DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    """Declarative base for all models."""
