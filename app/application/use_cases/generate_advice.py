91482bc97b6bd0d80f9a7d9c0b5dbdafadab6bad
hage9998 <lucashage23@gmail.com>
2026-07-03 16:13:40 -0300

    feat: create message object

--- app/application/use_cases/generate_advice.py
+++ app/application/use_cases/generate_advice.py
@@ -1,5 +1,13 @@
+from app.application.interfaces.advice_dto import AdviceRequestDTO, AdviceResponseDTO
+from app.domain.entities.message import Message, MessageRole
+
+_GENERATION_TRIGGER = (
+    "Conceda-me um novo conselho para minha jornada de hoje, ó sábio oráculo."
+)
+
+
 class GenerateAdviceUseCase:
     """ """
 
-    async def execute(self):
-        pass
+    async def execute(self, request: AdviceRequestDTO) -> AdviceResponseDTO:
+        messages = [Message(role=MessageRole.USER, content=_GENERATION_TRIGGER)]
